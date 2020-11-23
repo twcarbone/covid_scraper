d36cef607a9652d0afeb65ad9de381dd68ca6084
Tyler <tcarbone073@gmail.com>
2020-11-23 17:23:42 -0500

    add pie chart plotting

--- covid_scraper.py
+++ covid_scraper.py
@@ -40,8 +40,9 @@ import requests
 from bs4 import BeautifulSoup as bs
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
+from matplotlib import gridspec
 import numpy as np
-from covid_classes import COVID_Day as COVID_Day
+from covid_classes import COVID_Day
 
 
 # make an http request to the url
@@ -85,6 +86,7 @@ def parse_html(tag,soup):
     return covid_data
 
 
+
 def print_cases(covid_data,option):
     """
     Helper function for testing.
@@ -122,13 +124,80 @@ def print_cases(covid_data,option):
 
 
 
-def print_unique_locations(covid_data):
-    unique = []
+def get_locations(covid_data,total_num_cases,print_flag=False):
+    """
+    
+    """
+
+    # create dictionary
+    unique_locations = {}
     for day in covid_data:
         for location in day.location_list:
-            if not(location in unique):
-                unique.append(location)
-                print(location)
+            if not(location in unique_locations):
+                unique_locations[location] = 1
+            else:
+                unique_locations[location] += 1
+       
+    # make two lists (for plotting purposes)
+    location_list = []
+    location_count_list = []
+    for location,count in unique_locations.items():
+        location_list.append(location)
+        location_count_list.append(count)
+
+    # sort the lists by descending order
+    zipped_lists = zip(location_count_list,location_list)
+    sorted_pairs = sorted(zipped_lists,reverse=True)
+    tuples = zip(*sorted_pairs)
+    location_count_list,location_list = [list (tuple) for tuple in tuples]
+
+    # raise error if the number of cases don't match
+    if sum(location_count_list) != (total_num_cases-87):
+        raise Exception("Total number of cases does not match")
+
+    # compute percentage for each location
+    location_shares = []
+    for i,count in enumerate(location_count_list):
+        location_share = (count/sum(location_count_list))*100
+        location_shares.append(location_share)
+
+    # print to console if print_flag is True
+    if print_flag:
+        for i,location in enumerate(location_list):
+            # Quonset Point: 120/396 (30.3 %)
+            print(f"{location_list[i]}: ",end="") # Quonset Point: 
+            print(f"{location_count_list[i]}/{sum(location_count_list)}",end="") # 120/396
+            print(f" ({round(location_shares[i],2)} %)") # (30.3 %)
+
+    return location_list, location_count_list, location_shares
+
+
+
+def get_running_totals(covid_data):
+    """
+
+    """
+
+    date_list = []
+    cases_per_day_list = []
+    running_total_list = []
+    for i,day in enumerate(covid_data):
+
+        if day == covid_data[0]:
+            # if it is the first day of data, then there is no previous day and the method
+            # defaults to adding 87 to the current day's case count
+            day.update_running_total()
+        else:
+            # otherwise, add the running total from the previous day to today's case count
+            day.update_running_total(covid_data[i-1].running_total)
+
+        # make lists for plotting purposes
+        date_list.append(day.date_obj)
+        cases_per_day_list.append(day.num_cases)
+        running_total_list.append(day.running_total)
+
+    return date_list, cases_per_day_list, running_total_list
+
 
 
 def merge_day_list(list1,list2):
@@ -153,42 +222,35 @@ covid_data_pre = parse_html('pre',soup)
 covid_data_p = parse_html('p',soup) # October 19th & 23rd
 covid_data = merge_day_list(covid_data_p,covid_data_pre)
 
-print_cases(covid_data,1)
-print_unique_locations(covid_data)
-
-"""
-date_list = []
-cases_per_day_list = []
-running_total_list = []
-for i,day in enumerate(covid_data):
-
-    if day == covid_data[0]:
-        # if it is the first day of data, then there is no previous day and the method
-        # defaults to adding 87 to the current day's case count
-        day.update_running_total()
-    else:
-        # otherwise, add the running total from the previous day to today's case count
-        day.update_running_total(covid_data[i-1].running_total)
-
-    # make lists for plotting purposes
-    date_list.append(day.date_obj)
-    cases_per_day_list.append(day.num_cases)
-    running_total_list.append(day.running_total)
+# print_cases(covid_data,1)
+date_list, cases_per_day_list, running_total_list = get_running_totals(covid_data)
+loc_list, loc_count_list, loc_shares = get_locations(covid_data,running_total_list[-1],True)
 
 
 
 
-fig,ax = plt.subplots(2,figsize=(10,7))
+
+
+
+fig = plt.figure(figsize=(10,7))
+# fig,ax = plt.subplots(2,2,figsize=(10,7))
+spec = gridspec.GridSpec(ncols=2,nrows=2,width_ratios=[2,1])
+
 fig.suptitle("COVID Cases at Electric Boat")
-ax[0].plot(date_list,cases_per_day_list,marker="o")
-ax[1].plot(date_list,running_total_list,marker="o")
+
+ax0 = fig.add_subplot(spec[0,0])
+ax0.plot(date_list,cases_per_day_list,marker="o")
+ax0.set_ylabel("Cases Per Day")
+
+ax1 = fig.add_subplot(spec[1,0])
+ax1.plot(date_list,running_total_list,marker="o")
+ax1.set_ylabel("Running Total")
+
+ax2 = fig.add_subplot(spec[0,1])
+ax2.pie(loc_shares,labels=loc_list)
+# ax2.legend(labels=loc_list,loc='center right')
 
 plt.xticks(rotation=45)
-ax[0].set_ylabel("Cases Per Day")
-ax[1].set_ylabel("Running Total")
-
-ax[1].xaxis.set_major_locator(mdates.AutoDateLocator())
-
 plt.gcf().autofmt_xdate()
+
 plt.show()
-"""
