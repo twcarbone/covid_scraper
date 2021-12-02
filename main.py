a78e457e85ba106ae9fc1379d2312af2e8a2cf36
Tyler Carbone <tcarbone073@gmail.com>
2021-12-02 22:55:42 +0000

    Add SQL query examples

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ from scrape import *
 from crud import *
 from models import Case
 
+# Specific querying functions
+from sqlalchemy import func, and_, or_
+
 
 URL = 'https://eblanding.com/covid-19-case-report-summary/'
 
@@ -22,4 +25,24 @@ with session_scope() as s:
             )
         )
 
+    # SELECT case_num, date FROM cases WHERE dept='462'
+#    r = s.query(Case.case_num, Case.date).filter(Case.dept=='462').all()
+    
+    # SELECT * FROM cases WHERE dept='462' AND case_num>1000
+#     r = s.query(Case).filter(
+#             and_(
+#                 Case.dept=='462',
+#                 Case.case_num>1000
+#                 )
+#             ).all()
+   
+    # SELECT COUNT(case_num), dept FROM cases GROUP BY dept DESC LIMIT 15
+#     r = s.query(
+#             func.count(Case.case_num), Case.dept).\
+#             group_by(Case.dept).\
+#             order_by(func.count(Case.case_num).desc()).\
+#             limit(15)
+    
+    for item in r:
+        print(item)
 
